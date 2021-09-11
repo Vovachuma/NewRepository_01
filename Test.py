43a4ff08dcc3dad7945f294b744b77e8e25cfb62
VolodumurSolchanuck <volodumurSolchanuck@gmail.com>
2021-09-11 13:48:04 +0300

    add2

--- Test.py
+++ Test.py
@@ -1,5 +1,9 @@
-
-print("a")
-a=2727;
-print(a)
-
+number=29
+guess=int(input('введіть число:'))
+if guess==number:
+    print('ви виграли')
+elif guess<number:
+    print('загадане число більше')
+else:
+    print('загадане число менше')
+    print('завершено')
