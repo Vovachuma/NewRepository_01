29d5b88f7541421280760cb25411d5a5204c3061
Vlad86 <90153427+Vovachuma@users.noreply.github.com>
2022-02-05 13:35:08 +0200

    Delete Test.py

--- Test.py
+++
@@ -1,9 +0,0 @@
-number=29
-guess=int(input('введіть число:'))
-if guess==number:
-    print('ви виграли')
-elif guess<number:
-    print('загадане число більше')
-else:
-    print('загадане число менше')
-    print('завершено')
